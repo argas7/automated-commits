9f8cf7af0fb9168d64a1ee38364965ab41e1375d
argas7 <argas@cin.ufpe.br>
2020-06-18 22:27:00 -0300

    Feature(master): add customizable commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,31 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+// module.exports = { extends: ['@commitlint/config-conventional'] };
+
+module.exports = {
+	parserPreset: './commitlint.config.js',
+	rules: {
+		'body-leading-blank': [1, 'always'],
+		'body-max-line-length': [2, 'always', 100],
+		'footer-leading-blank': [1, 'always'],
+		'footer-max-line-length': [2, 'always', 100],
+		'header-max-length': [2, 'always', 100],
+		'scope-case': [2, 'always', 'kebab-case'],
+		'subject-case': [
+			2,
+			'never',
+			['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+		],
+		'subject-empty': [2, 'never'],
+		'subject-full-stop': [2, 'never', '.'],
+		'type-case': [2, 'always', 'sentence-case'],
+		'type-empty': [2, 'never'],
+		'type-enum': [
+			2,
+			'always',
+			[
+        'Feature',
+        'Bugfix',
+        'Hotfix'
+			]
+		]
+	}
+};
